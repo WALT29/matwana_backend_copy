98f6a77bc26fe38d15aa30aab6fe7e84cde0a904
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-10-29 16:56:53 +0300

    modified add

--- server/app.py
+++ server/app.py
@@ -506,5 +506,6 @@ class Userparcels(Resource):
 api.add_resource(Userparcels,'/assignments/<int:id>') 
             
         
-if __name__==("__main__"):
-    app.run(port=5555,debug=True)       
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5555))
+    app.run(host="0.0.0.0", port=port, debug=True)
